028274281cf9931e19be8e2bac3c1a72815e5f93
Reinier Pelayo <reinier.pelayo@peak10.com>
2016-06-16 13:15:02 -0400

    Initial commit

---
+++ .bashrc
@@ -0,0 +1,8 @@
+# .bashrc
+[ -n "$PS1" ] && source ~/.bash_profile;
+
+# Startup commands
+cd /var/www/piton
+
+# Update PATH
+PATH=~/.composer/vendor/bin/:$PATH
---
+++ .expectrum.sh
@@ -0,0 +1,44 @@
+#! /bin/bash
+# A script to make using 256 colors in zsh less painful.
+# P.C. Shyamshankar <sykora@lucentbeing.com>
+# Copied from http://github.com/sykora/etc/blob/master/zsh/functions/spectrum/
+
+declare -Ax FX FG BG
+
+FX=(
+    [reset]="\e[0m"
+    [bold]="\e[1m" [no-bold]="\e[21m"
+    [italic]="\e[3m" [no-italic]="\e[23m"
+    [underline]="\e[4m" [no-underline]="\e[24m"
+    [blink]="\e[5m" [no-blink]="\e[25m"
+    [reverse]="\e[7m" [no-reverse]="\e[27m"
+)
+
+for color in {000..255}; do
+    FG[$color]="\e[38;5;${color}m"
+    BG[$color]="\e[48;5;${color}m"
+done
+
+
+ZSH_SPECTRUM_TEXT=${ZSH_SPECTRUM_TEXT:-Arma virumque cano Troiae qui primus ab oris}
+
+# Show all 256 colors with color number
+function spectrum_ls() {
+  for code in {000..255}; do
+    echo -e "$code: ${FG['$code']}$ZSH_SPECTRUM_TEXT ${FG[reset]}"
+  done
+}
+
+# Show all 256 colors where the background is set to specific color
+function spectrum_bls() {
+  for code in {000..255}; do
+    echo -e "${BG[$code]}$code: $ZSH_SPECTRUM_TEXT ${FX[reset]}"
+  done
+}
+
+# Default colors
+BOLD="${FX[bold]}"
+GREEN="${FG[120]}"
+BLUE="${FG[110]}"
+RED="${FG[167]}"
+RESET="${FX[reset]}"
