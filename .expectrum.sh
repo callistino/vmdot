68adde7158daace44f8a08f828b02a71046b624c
Reinier Pelayo <reinier.pelayo@peak10.com>
2016-07-28 17:48:50 -0500

    • Support Bash 3 by removing associative arrays. • Updated colors to use material UI

--- .expectrum.sh
+++ .expectrum.sh
@@ -3,42 +3,45 @@
 # P.C. Shyamshankar <sykora@lucentbeing.com>
 # Copied from http://github.com/sykora/etc/blob/master/zsh/functions/spectrum/
 
-declare -Ax FX FG BG BOLD GREEN BLUE RED RESET
+typeset -ax FX FG BG BOLD GREEN BLUE RED RESET
 
-FX=(
-    [reset]="\e[0m"
-    [bold]="\e[1m" [no-bold]="\e[21m"
-    [italic]="\e[3m" [no-italic]="\e[23m"
-    [underline]="\e[4m" [no-underline]="\e[24m"
-    [blink]="\e[5m" [no-blink]="\e[25m"
-    [reverse]="\e[7m" [no-reverse]="\e[27m"
-)
+FX_RESET="\033[0m"
+FX_BOLD="\e[1m"
+FX_NO_BOLD="\e[21m"
+FX_ITALIC="\e[3m"
+FX_NO_ITALIC="\e[23m"
+FX_UNDERLINE="\e[4m"
+FX_NO_UNDERLINE="\e[24m"
+FX_BLINK="\e[5m"
+FX_NO_BLINK="\e[25m"
+FX_REVERSE="\e[7m"
+FX_NO_REVERSE="\e[27m"
+
 
 for color in {000..255}; do
     FG[$color]="\e[38;5;${color}m"
     BG[$color]="\e[48;5;${color}m"
 done
 
-
-ZSH_SPECTRUM_TEXT=${ZSH_SPECTRUM_TEXT:-Arma virumque cano Troiae qui primus ab oris}
+SPECTRUM_TEXT=${SPECTRUM_TEXT:-Arma virumque cano Troiae qui primus ab oris}
 
 # Show all 256 colors with color number
 function spectrum_ls() {
   for code in {000..255}; do
-    echo -e "$code: ${FG['$code']}$ZSH_SPECTRUM_TEXT ${FG[reset]}"
+    printf "${FG[$code]}$code: $SPECTRUM_TEXT ${FX_RESET}\n"
   done
 }
 
 # Show all 256 colors where the background is set to specific color
 function spectrum_bls() {
   for code in {000..255}; do
-    echo -e "${BG[$code]}$code: $ZSH_SPECTRUM_TEXT ${FX[reset]}"
+    printf "${BG[$code]}$code: $SPECTRUM_TEXT ${FX_RESET}\n"
   done
 }
 
 # Default colors
-BOLD="${FX[bold]}"
-GREEN="${FG[120]}"
+BOLD="${FX_BOLD}"
+GREEN="${FG[193]}"
 BLUE="${FG[110]}"
 RED="${FG[167]}"
-RESET="${FX[reset]}"
+RESET="${FX_RESET}"
